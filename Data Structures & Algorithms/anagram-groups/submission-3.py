1b493d4deb01c7448573040b018bf8f4217ed6db
Liyi Huang <liyi.huang@isovalent.com>
2026-04-10 17:11:10 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        res = {}
+        for s in strs:
+            c_count = {}
+            for c in s:
+                if c not in c_count:
+                    c_count[c] = 1
+                else:
+                    c_count[c] += 1
+            key = frozenset(c_count.items())
+
+            if key not in res:
+                res[key] = []
+            res[key].append(s)
+            
+        return list(res.values())
